13d1a4300813638cdc773a1da91130060a0b047e
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-05-17 00:41:49 -0230

    Add `soaap-combine` for unions, intersections...
    This tool loads any number of binary-encoded CallGraph files and folds them together with an operation like union, intersect, etc.

---
+++ cmd/soaap-combine/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/CTSRD-SOAAP/gosoaap"
+)
+
+var (
+	output = flag.String("output", "-", "output file")
+
+	operation = flag.String("operation", "union",
+		"the combining operation to perform (union|intersection)")
+
+	intersectionDepth = flag.Int("intersection-depth", 3,
+		"how many calls to trace back from a leaf node when looking"+
+			" for call graph intersections")
+)
+
+func main() {
+	flag.Parse()
+
+	//
+	// Open input files:
+	//
+	if len(flag.Args()) < 2 {
+		printUsage()
+		return
+	}
+
+	graphs := make([]soaap.CallGraph, 0, 2)
+	for _, filename := range flag.Args() {
+		f, err := os.Open(filename)
+		if err != nil {
+			die("error opening '%s': %s", filename, err)
+		}
+
+		graph, err := soaap.LoadGraph(f, report)
+		if err != nil {
+			die("error loading graph from '%s': %s", filename, err)
+		}
+
+		graphs = append(graphs, graph)
+	}
+
+	//
+	// Open output file:
+	//
+	out, err := os.Create(*output)
+	if err != nil {
+		die("error opening '%s': %s", *output, err)
+	}
+
+	//
+	// Apply the requested combining operation:
+	//
+	graph := graphs[0]
+	for _, g := range graphs[1:] {
+		switch *operation {
+		case "addintersecting":
+			err = graph.AddIntersecting(g, *intersectionDepth)
+
+		case "intersection":
+			graph, err = graph.Intersect(g, *intersectionDepth)
+
+		case "union":
+			err = graph.Union(g)
+
+		default:
+			die("Unknown combining operation: '%s'", *operation)
+		}
+	}
+
+	if err != nil {
+		die("error applying '%s': %s", *operation, err)
+	}
+
+	nodes, edges := graph.Size()
+	fmt.Printf("Final graph has %d nodes and %d edges.\n", nodes, edges)
+
+	err = graph.Save(out)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error saving: %s\n", err)
+	}
+}
+
+func die(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", args...)
+	os.Exit(1)
+}
+
+func printUsage() {
+	fmt.Fprintf(os.Stderr,
+		"Usage:  soaap-combine [options] <input files>\n\n")
+
+	fmt.Fprintf(os.Stderr, "Options:\n")
+	flag.PrintDefaults()
+}
+
+func report(progress string) {
+	fmt.Println(progress)
+	os.Stdout.Sync()
+}
