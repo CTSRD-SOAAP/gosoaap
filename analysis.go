87a3aaed0079313d816a07a9e93dcaf17d592550
Jonathan Anderson <jonathan.anderson@ieee.org>
2015-08-11 16:25:38 -0230

    Add ability to filter graphs by leaf node name.

--- analysis.go
+++ analysis.go
@@ -1,6 +1,10 @@
 package soaap
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 // A function that can produce a CallGraph when given a CallGraph.
 type Analyser func(*CallGraph) (CallGraph, error)
@@ -13,13 +17,7 @@ type Analyser func(*CallGraph) (CallGraph, error)
 //   * "+graphtype": union with "graphtype" from the Results
 //   * "^graphtype": intersection with "graphtype" from the Results
 //   * ".graphtype": union-of-intersection with "graphtype"
-//   * ":spec": filter leaf nodes according to "spec":
-//     * "+name": keep a leaf node
-//     * "*": keep all current leaf nodes (used together with '-', below)
-//     * "-name": remove a leaf node
-//     * examples:
-//       * ":ALL:-foo:-bar" keeps all leaf nodes except "foo" and "bar"
-//       * ":+foo:+bar" keeps only "foo" and "bar" leaf nodes
+//   * ":spec": filter leaf nodes according to "spec" (see Filter)
 //
 // where "graphtype" can be:
 //   * vuln: the callgraph of previously-vulnerable code
@@ -53,6 +51,10 @@ func ApplyAnalysis(spec string, cg *CallGraph, results *Results,
 				return *cg, err
 			})
 
+	case ':':
+		report("Filtering with '" + spec + "'")
+		return Filter(*cg, spec[1:])
+
 	default:
 		report("Adding " + spec)
 		return extractAndCombine(spec, results, report, union)
@@ -72,4 +74,65 @@ func extractAndCombine(graphname string, r *Results, report func(string),
 		graphname, nodes, edges, flows))
 
 	return analyse(&g)
+}
+
+//
+// Filter a graph according to a colon-separated list of filter specifications,
+// where each element can be:
+//
+//  * "*": add all leaf nodes in the graph
+//  * "+regex": keep leaf nodes that match a pattern
+//  * "-regex": remove leaf nodes that match a pattern
+//
+// Examples:
+//
+//  ":*:-foo:-bar" keeps all leaf nodes except "foo" and "bar"
+//
+//  ":+.*foo.*:+.*bar.*" keeps only those leaf nodes (plus ancestors)
+//  with "foo" and "bar" in their names
+//
+func Filter(cg CallGraph, spec string) (CallGraph, error) {
+	leaves := make(strset)
+
+	for _, s := range strings.Split(spec, ":") {
+		if s == "*" {
+			leaves = leaves.Union(cg.leaves)
+			continue
+		}
+
+		var add bool
+		switch s[0] {
+		case '+':
+			add = true
+
+		case '-':
+			add = false
+
+		default:
+			return CallGraph{},
+				fmt.Errorf("unknown filter clause: '%s'", s)
+		}
+
+		pattern, err := regexp.Compile(s[1:])
+		if err != nil {
+			return cg, err
+		}
+
+		for leaf := range cg.leaves {
+			if pattern.MatchString(leaf) {
+				if add {
+					leaves.Add(leaf)
+				} else {
+					leaves.Remove(leaf)
+				}
+			}
+		}
+	}
+
+	keep := make(strset)
+	for leaf := range leaves {
+		keep = keep.Union(cg.Ancestors(leaf, -1))
+	}
+
+	return cg.Filter(keep), nil
 }
